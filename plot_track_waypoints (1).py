a0cd4e38e75ef151db67bef76658234f0436456b
caelinsutch <32991829+caelinsutch@users.noreply.github.com>
2019-07-26 10:39:05 -0700

    Add files via upload

---
+++ plot_track_waypoints (1).py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+track_name = "New_York_Track"
+absolute_path = "."
+waypoints = np.load("%s/tracks/%s.npy" % (absolute_path, track_name))
+
+print("Number of waypoints = " + str(waypoints.shape[0]))
+for i, point in enumerate(waypoints):
+    waypoint = (point[2], point[3])
+    plt.scatter(waypoint[0], waypoint[1])
+    print("Waypoint " + str(i) + ": " + str(waypoint))
+
+plt.show()
